2138b57cef25196006a1f6100f5209780200e7df
Louis Pascuzzi <pascuzzilouis@gmail.com>
2019-09-26 20:48:49 -0400

    ajout d une listview et creation de la classe ProduitAdapter

--- android_foodwatcher/app/src/main/java/ca/qc/cgmatane/foodwatcher/vue/home/HomeFragment.java
+++ android_foodwatcher/app/src/main/java/ca/qc/cgmatane/foodwatcher/vue/home/HomeFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -11,10 +12,15 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import ca.qc.cgmatane.foodwatcher.R;
 
 public class HomeFragment extends Fragment {
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager layoutManager;
 
     private HomeViewModel homeViewModel;
 
@@ -30,6 +36,37 @@ public class HomeFragment extends Fragment {
                 textView.setText(s);
             }
         });
+        super.onCreate(savedInstanceState);
+        recyclerView = (RecyclerView) root.findViewById(R.id.my_recycler_view);
+
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        recyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+        layoutManager = new LinearLayoutManager(root.getContext());
+        recyclerView.setLayoutManager(layoutManager);
+
+        // specify an adapter (see also next example)
+        mAdapter = new RecyclerView.Adapter() {
+            @NonNull
+            @Override
+            public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                return null;
+            }
+
+            @Override
+            public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+
+            }
+
+            @Override
+            public int getItemCount() {
+                return 0;
+            }
+        };
+        recyclerView.setAdapter(mAdapter);
+
         return root;
     }
 }
