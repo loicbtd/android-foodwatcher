855eec2db32fc5fbf23036ea70c5ce9347b693ab
EstevanGAY <estevan.gay@gmail.com>
2019-09-30 02:58:36 -0400

    Ajout de la carte

--- android_foodwatcher/app/src/main/java/ca/qc/cgmatane/foodwatcher/view/FindStoreActivity.java
+++ android_foodwatcher/app/src/main/java/ca/qc/cgmatane/foodwatcher/view/FindStoreActivity.java
@@ -1,21 +1,104 @@
 package ca.qc.cgmatane.foodwatcher.view;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 
 import ca.qc.cgmatane.foodwatcher.R;
 
-public class FindStoreActivity extends MasterActivity {
+public class FindStoreActivity extends MasterActivity implements OnMapReadyCallback {
 
-    //TODO: create and add controller as attribute
+    public Location currentLocation;
+    public FusedLocationProviderClient fusedLocationProviderClient;
+
+    private static final int REQUEST_CODE = 101;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         super.configureActivityContent(R.layout.view_find_store);
-//        super.configureToolbarMenu(R.menu.view_sample_toolbar);
-//        toolbar.setOnMenuItemClickListener();
 
-        navigationView.getMenu().findItem(R.id.activity_master_drawer_action_find_store).setChecked(true); //TODO: improve check verification system
-        // TODO: call the controller onCreate method
+        navigationView.getMenu().findItem(R.id.activity_master_drawer_action_find_store).setChecked(true);
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+        fetchLocation();
+
     }
+
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
+        LatLng latLng = new LatLng(currentLocation.getLatitude(),currentLocation.getLongitude());
+        MarkerOptions markerOptions = new MarkerOptions().position(latLng).title("You");
+
+        googleMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
+        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng,15.0f));
+        googleMap.addMarker(markerOptions);
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_CODE:
+
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    fetchLocation();
+
+                }
+
+                break;
+        }
+    }
+
+    private void fetchLocation() {
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(this,new String[]{
+                    Manifest.permission.ACCESS_FINE_LOCATION
+            },REQUEST_CODE);
+
+            return;
+
+        }
+
+        Task<Location> task = fusedLocationProviderClient.getLastLocation();
+        task.addOnSuccessListener(new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                if (location != null){
+
+                    currentLocation = location;
+//                    Toast.makeText(getApplicationContext(),currentLocation.getLatitude()+", "+currentLocation.getLongitude(),Toast.LENGTH_SHORT).show();
+                    SupportMapFragment supportMapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.fragment_map);
+
+                    supportMapFragment.getMapAsync(FindStoreActivity.this);
+
+                }
+            }
+        });
+
+    }
+
+
 }
