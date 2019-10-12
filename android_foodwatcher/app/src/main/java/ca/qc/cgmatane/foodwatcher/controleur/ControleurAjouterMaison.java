e6ee5afa381b8746099da14630cd77c4842d23ef
Louis Pascuzzi <pascuzzilouis@gmail.com>
2019-10-12 12:52:38 -0400

    implementation du controleur et de l'interface de la vue AjouterMaison

---
+++ android_foodwatcher/app/src/main/java/ca/qc/cgmatane/foodwatcher/controleur/ControleurAjouterMaison.java
@@ -0,0 +1,73 @@
+package ca.qc.cgmatane.foodwatcher.controleur;
+
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import ca.qc.cgmatane.foodwatcher.vue.AjouterMaison;
+
+
+public class ControleurAjouterMaison implements Controleur {
+
+    private AjouterMaison vue;
+
+    public ControleurAjouterMaison(AjouterMaison vue) {
+       this.vue = vue;
+    }
+
+    public void actionRetourActiviteMaitresse(){
+        vue.retourActiviteMaitresse();
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    @Override
+    public boolean equals(@Nullable Object obj) {
+        return super.equals(obj);
+    }
+
+    @NonNull
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
+
+    @Override
+    protected void finalize() throws Throwable {
+        super.finalize();
+    }
+
+    @Override
+    public void onCreate(Context applicationContext) {
+
+    }
+
+    @Override
+    public void onPause() {
+
+    }
+
+    @Override
+    public void onResume() {
+
+    }
+
+    @Override
+    public void onDestroy() {
+
+    }
+
+    @Override
+    public void onActivityResult(int activite) {
+
+    }
+
+    @Override
+    public void onBackPressed() {
+
+    }
+}
--- android_foodwatcher/app/src/main/java/ca/qc/cgmatane/foodwatcher/vue/AjouterMaison.java
+++ android_foodwatcher/app/src/main/java/ca/qc/cgmatane/foodwatcher/vue/AjouterMaison.java
@@ -1,14 +1,17 @@
 package ca.qc.cgmatane.foodwatcher.vue;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 import ca.qc.cgmatane.foodwatcher.R;
+import ca.qc.cgmatane.foodwatcher.controleur.ControleurAjouterMaison;
 
-public class AjouterMaison extends ActiviteMaitresse {
+public class AjouterMaison extends ActiviteMaitresse implements AjouterMaisonVue {
     Button boutonAjouterMaison;
     Button boutonAnnuler;
+    ControleurAjouterMaison controleur;
     //TODO: create and add controller as attribute
 
     @Override
@@ -17,21 +20,27 @@ public class AjouterMaison extends ActiviteMaitresse {
         super.configureActivityContent(R.layout.vue_ajouter_maison);
 //        super.configureToolbarMenu(R.menu.vue_exemple_toolbar);
 //        toolbar.setOnMenuItemClickListener();
+        controleur = new ControleurAjouterMaison(this);
         boutonAjouterMaison = findViewById(R.id.bouton_ajouter_maison);
         boutonAjouterMaison.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-//                startActivity(new Intent(this, ActiviteMaitresse.class));
+                controleur.actionRetourActiviteMaitresse();
             }
         });
         boutonAnnuler = findViewById(R.id.bouton_annuler_maison);
         boutonAnnuler.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-//                startActivity(new Intent(this, ActiviteMaitresse.class));
+                controleur.actionRetourActiviteMaitresse();
             }
         });
         navigationView.getMenu().findItem(R.id.activity_master_drawer_action_add_home).setChecked(true); //TODO: improve check verification system
         // TODO: call the controller onCreate method
     }
+
+    public void retourActiviteMaitresse(){
+        this.finish();
+    }
+
 }
