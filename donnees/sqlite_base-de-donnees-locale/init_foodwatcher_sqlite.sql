2adc0570695e80c9cae03ae00c9723029de7bc66
loicbtd <loic.bert.marcel@gmail.com>
2019-10-13 23:39:33 -0400

    simplification de la base de données

--- donnees/sqlite_base-de-donnees-locale/init_foodwatcher_sqlite.sql
+++ donnees/sqlite_base-de-donnees-locale/init_foodwatcher_sqlite.sql
@@ -6,38 +6,10 @@ DROP TABLE IF EXISTS categorie_produit;
 DROP TABLE IF EXISTS emplacement;
 DROP TABLE IF EXISTS stock_compose_utilisateur;
 DROP TABLE IF EXISTS stock;
-DROP TABLE IF EXISTS utilisateur;
-
-
-CREATE TABLE utilisateur(
-    id_utilisateur integer PRIMARY KEY,
-    email text,
-    mot_de_passe text,
-    nom text,
-    prenom text,
-    telephone text,
-    date_naissance date
-);
 
 CREATE TABLE stock(
     id_stock integer PRIMARY KEY,
     etiquette text
-);
-
-CREATE TABLE stock_compose_utilisateur(
-    id_stock integer NOT NULL,
-    id_utilisateur integer NOT NULL,
-    PRIMARY KEY(id_stock,id_utilisateur),
-    CONSTRAINT stock_stock_compose_utilisateur_fk
-        FOREIGN KEY (id_stock)
-        REFERENCES stock(id_stock)
-        ON DELETE CASCADE
-        ON UPDATE CASCADE,
-    CONSTRAINT utilisateur_stock_compose_utilisateur_fk
-        FOREIGN KEY (id_utilisateur)
-        REFERENCES utilisateur(id_utilisateur)
-        ON DELETE CASCADE
-        ON UPDATE CASCADE
 );
 
 CREATE TABLE emplacement(
@@ -59,10 +31,8 @@ CREATE TABLE produit(
     id_produit integer PRIMARY KEY,
     gencode integer,
     etiquette text,
-    nombre_jour_conservation integer,
-    chemin_image text,
-    id_unite_quantite integer NOT NULL,
-    id_categorie_produit integer NOT NULL,
+    id_unite_quantite integer,
+    id_categorie_produit integer,
     CONSTRAINT unite_quantite_produit_fk
         FOREIGN KEY (id_unite_quantite)
         REFERENCES unite_quantite(id_unite_quantite)
@@ -76,10 +46,11 @@ CREATE TABLE produit(
 );
 
 CREATE TABLE stock_compose_produit(
-    id_produit integer NOT NULL,
-    id_stock integer NOT NULL,
+    id_produit integer,
+    id_stock integer,
+    quantite real,
     id_emplacement integer,
-    present_liste_course boolean NOT NULL,
+    present_liste_course integer,
     PRIMARY KEY(id_produit,id_stock),
     CONSTRAINT produit_stock_compose_produit_fk
         FOREIGN KEY (id_produit)
@@ -94,19 +65,6 @@ CREATE TABLE stock_compose_produit(
     CONSTRAINT emplacement_stock_compose_produit_fk
         FOREIGN KEY (id_emplacement)
         REFERENCES emplacement(id_emplacement)
-        ON DELETE CASCADE
-        ON UPDATE CASCADE
-);
-
-CREATE TABLE historique_stock(
-    id_historique_stock integer PRIMARY KEY,
-    quantite float NOT NULL,
-    date_saisie date NOT NULL,
-    id_produit integer NOT NULL,
-    id_stock integer NOT NULL,
-    CONSTRAINT stock_compose_produit_historique_stock_fk
-        FOREIGN KEY (id_produit,id_stock)
-        REFERENCES stock_compose_produit(id_produit,id_stock)
         ON DELETE CASCADE
         ON UPDATE CASCADE
 );
