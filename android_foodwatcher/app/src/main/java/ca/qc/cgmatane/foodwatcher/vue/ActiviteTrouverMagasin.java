8fd885eb51a72392efc46c30401ad486f9a9aa1b
EstevanGAY <estevan.gay@gmail.com>
2019-10-15 20:04:38 -0400

    ajout des mock data pour les lieux où faire les courses et modification de l'icône du marqueur de la position du l'utilisateur

--- android_foodwatcher/app/src/main/java/ca/qc/cgmatane/foodwatcher/vue/ActiviteTrouverMagasin.java
+++ android_foodwatcher/app/src/main/java/ca/qc/cgmatane/foodwatcher/vue/ActiviteTrouverMagasin.java
@@ -14,10 +14,14 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import ca.qc.cgmatane.foodwatcher.R;
 
@@ -46,11 +50,69 @@ public class ActiviteTrouverMagasin extends ConteneurPrincipal implements OnMapR
     public void onMapReady(GoogleMap googleMap) {
 
         LatLng latLng = new LatLng(currentLocation.getLatitude(),currentLocation.getLongitude());
-        MarkerOptions markerOptions = new MarkerOptions().position(latLng).title("You");
+        MarkerOptions markerOptions = new MarkerOptions()
+                .position(latLng)
+                .title("Vous")
+                .icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_position));
 
         googleMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
         googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng,15.0f));
         googleMap.addMarker(markerOptions);
+
+        mockDataMarker(googleMap);
+
+    }
+
+    private void mockDataMarker(GoogleMap googleMap) {
+
+        List<String> listeNomsLieux = new ArrayList<>();
+        List<LatLng> listeCoordonnees = new ArrayList<>();
+
+        listeCoordonnees.add(new LatLng(48.840892, -67.519760));
+        listeNomsLieux.add("Boni-Soir - Mini marché du Lac inc.");
+
+        listeCoordonnees.add(new LatLng(48.851537, -67.511659));
+        listeNomsLieux.add("IGA Marché DesRosiers inc.");
+
+        listeCoordonnees.add(new LatLng(48.851712, -67.508867));
+        listeNomsLieux.add("Maxi");
+
+        listeCoordonnees.add(new LatLng(48.845465, -67.529805));
+        listeNomsLieux.add("shoppers drug mart");
+
+        listeCoordonnees.add(new LatLng(48.846317, -67.532576));
+        listeNomsLieux.add("Dépanneur Labonté");
+
+        listeCoordonnees.add(new LatLng(48.840892, -67.519760));
+        listeNomsLieux.add("Boni-Soir - Mini marché du Lac inc.");
+
+        listeCoordonnees.add(new LatLng(48.847176, -67.529925));
+        listeNomsLieux.add("Boulangerie Toujours Dimanche Inc");
+
+        listeCoordonnees.add(new LatLng(48.852327, -67.537126));
+        listeNomsLieux.add("Metro Plus Matane");
+
+        listeCoordonnees.add(new LatLng(48.852249, -67.537416));
+        listeNomsLieux.add("Super C");
+
+        listeCoordonnees.add(new LatLng(48.850117, -67.547238));
+        listeNomsLieux.add("Poissonnerie du Phare Ouest");
+
+        listeCoordonnees.add(new LatLng(48.843929, -67.556156));
+        listeNomsLieux.add("Walmart Matane Store");
+
+        listeCoordonnees.add(new LatLng(48.839684, -67.540591));
+        listeNomsLieux.add("ÉPICERIE-DÉPANNEUR R. ST-GELAIS INC.");
+
+        MarkerOptions markerOptions;
+        for (int iterateur = 0; iterateur < listeCoordonnees.size(); iterateur++){
+
+            markerOptions = new MarkerOptions()
+                    .position(listeCoordonnees.get(iterateur))
+                    .title(listeNomsLieux.get(iterateur));
+            googleMap.addMarker(markerOptions);
+
+        }
 
     }
 
